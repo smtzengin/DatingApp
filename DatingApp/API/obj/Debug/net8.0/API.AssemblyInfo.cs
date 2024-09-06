be6fb562ddc268c5aab75b3dd9895b784162fc60
Samet Zengin <smt.zngn02@gmail.com>
2024-09-06 14:42:40 +0300

    add user, register and login dto and add jwt token creator  add api controllers

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5db0cfa82be72e5c930b7da4cd9e3513d157a902")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a24ef7364a55ab734871aa89ed8636ac46fe30")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

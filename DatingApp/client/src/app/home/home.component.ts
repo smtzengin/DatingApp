f95eb4756d2933f789f5cf4522aa23aaeefd7a31
Samet Zengin <smt.zngn02@gmail.com>
2024-09-10 12:35:37 +0300

    building the user interface

--- DatingApp/client/src/app/home/home.component.ts
+++ DatingApp/client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +8,10 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent{
   
-  http = inject(HttpClient);
   registerMode = false;
-  users:any;
 
-  ngOnInit(): void {
-   this.getUsers()
-  }
   registerToggle(){
     this.registerMode = !this.registerMode;
   }
@@ -25,11 +19,4 @@ export class HomeComponent implements OnInit{
     this.registerMode = event;
   }
 
-  getUsers(){
-    this.http.get("https://localhost:5001/api/users").subscribe({
-      next: response => this.users = response,
-      error : error => console.log(error),
-      complete: () => console.log("Request has completed")
-    })
-  }
 }
